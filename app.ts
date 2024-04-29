c24514c7e1d9b1910c55472203bf1241a00e4d5f
zchengfa <1522977814@qq.com>
2024-04-29 19:06:03 +0800

    feat(添加获取朋友圈数据接口):

--- app.ts
+++ app.ts
@@ -44,3 +44,50 @@ require('./router/index')(app,router,pool,redis)
 
 require('./router/loginRegister')(app,router,redis,pool)
 
+
+
+//MySQL写入朋友圈数据测试
+// app.post('/saveMoments',(req:any,res:any)=>{
+//     const {generateID} = require('./util/util')
+//     let params = req.body,moments_id = generateID()
+//
+//     //chat_moments
+//     let promiseOne = new Promise((resolve, reject)=>{
+//         let insertChatMoments = pool.self_query.insert('chat_moments','user_id,moments_id,moments_text,send_time',`${params.user_id},${moments_id},'${params.text}',${new Date().getTime()}`)
+//         pool.query(insertChatMoments,(err:any)=>{
+//             if(err){
+//                reject(err)
+//             }
+//             else{
+//                 resolve(true)
+//             }
+//         })
+//     })
+//
+//     //moments_images
+//     let promiseTwo = new Promise((resolve, reject)=>{
+//         let insertValues:any[] = []
+//         params.images.forEach((item:any)=>{
+//             insertValues.push({
+//                 moments_id:moments_id,
+//                 image:item
+//             })
+//         })
+//         let insertMomentsImages = pool.self_query.insert('moments_images','moments_id,image',insertValues,true)
+//         pool.query(insertMomentsImages,(err:any)=>{
+//             if(err){
+//                 reject(err)
+//             }
+//             else{
+//                 resolve(true)
+//             }
+//         })
+//     })
+//
+//     Promise.all([promiseOne, promiseTwo]).then((r:any)=>{
+//         res.send('success')
+//     }).catch((e:any)=>{
+//         res.send('failed')
+//     })
+// })
+
--- router/index.ts
+++ router/index.ts
@@ -1,3 +1,5 @@
+import {it} from "node:test";
+
 const multer = require('multer')
 const path = require('path')
 const {encodeImgBase64} = require('../util/util')
@@ -161,5 +163,175 @@ module.exports = (app:any,router:any,pool:any,redis:any)=>{
 
   })
 
+  router.post('/getChatMoments',(req:any,res:any)=>{
+    let user_id = req.body.user_id,time = new Date().getTime(),thresholdTime = 3*24*60*60*1000
+    /**
+     * 获取用户和用户好友发过的朋友圈数据
+     * 1.获取当前用户的user_id,username,avatar数据
+     * 2.获取用户好友id存储到数组中
+     * 3.根据id列表以及当前时间来获取距离当前时间多久之前的朋友圈数据（要获取用户和朋友信息，朋友圈文字、图片，点赞人以及评论数据）
+     */
+    //获取与当前用户相关的好友信息
+    let userSelect = pool.self_query.selectFields('users_association','friend_id',`user_id = ${user_id}`)
+    let userSelectPromise =  ()=>{
+      return new Promise((resolve, reject)=>{
+        pool.query(userSelect,(err:any,ids:any)=>{
+          if(err){
+            reject(err)
+          }
+          else{
+            let data:any[] = []
+            ids.map((item:any)=>{
+              data.push(item['friend_id'])
+            })
+            data.unshift(user_id)
+            resolve(data)
+          }
+        })
+      })
+    }
+
+    //获取自己和好友发送过的朋友圈数据
+    function getChatMomentsData(user_id:number | string){
+
+      function queryPool(select:any,resolve:any, reject:any){
+        pool.query(select,(err:any,u:any)=>{
+          if(err){
+            reject(err)
+          }
+          else{
+            resolve(u)
+          }
+        })
+      }
+      //获取用户信息
+      let selectUserInfoPromise = ()=>{
+        return new Promise((resolve, reject)=>{
+          let select = pool.self_query.selectFields('users','user_id,username,avatar',`user_id = ${user_id}`)
+          queryPool(select,resolve,reject)
+        })
+      }
+
+      //获取用户朋友圈text
+      let selectUserMomentsPromise = ()=>{
+        return new Promise((resolve, reject)=>{
+          let select = pool.self_query.selectAll('chat_moments',`user_id = ${user_id}`)
+          queryPool(select,resolve,reject)
+        })
+      }
+
+      //获取朋友圈images
+      let selectMomentsImagePromise = (moments_id:number)=>{
+        return new Promise((resolve, reject)=>{
+          let select = pool.self_query.selectFields('moments_images','image',`moments_id = ${moments_id}`)
+          queryPool(select,resolve,reject)
+        })
+      }
+
+      //获取朋友圈点赞者
+      let selectMomentsLikedPromise = (moments_id:number)=>{
+        return new Promise((resolve, reject)=>{
+          let select = pool.self_query.selectFields('moments_liked','like_user_id',`moments_id = ${moments_id}`)
+          queryPool(select,resolve,reject)
+        })
+      }
+
+      //获取朋友圈评论
+      let selectMomentsCommentsPromise = (moments_id:number)=>{
+        return new Promise((resolve, reject)=>{
+          let select = pool.self_query.selectFields('moments_comments','comments_user_id,comments_id,comments_text',`moments_id = ${moments_id}`)
+          queryPool(select,resolve,reject)
+        })
+      }
+
+      //获取点赞者或评论者的昵称
+      let selectUserNamePromise = (user_id:number)=>{
+        return new Promise((resolve, reject)=>{
+          let select = pool.self_query.selectFields('users','username',`user_id = ${user_id}`)
+          queryPool(select,resolve,reject)
+        })
+      }
+
+      return new Promise((resolve, reject)=>{
+        Promise.all([selectUserInfoPromise(),selectUserMomentsPromise()]).then((r:any)=>{
+          //若有朋友圈数据
+          if(r[1].length){
+            let moments:any = {}
+            moments['user'] = r[0][0]
+            moments['location'] = r[1][0]['location']
+            //删除不需要的数据
+            delete r[1][0]['id']
+            delete r[1][0]['user_id']
+            delete r[1][0]['location']
+            moments['content'] = r[1][0]
+            moments['content']['images'] = []
+            moments['liked'] = []
+            moments['comments'] = []
+            //通过moments_id获取朋友圈images、liked、comments数据
+            Promise.all([selectMomentsImagePromise(moments['content']['moments_id']),selectMomentsLikedPromise(moments['content']['moments_id']),selectMomentsCommentsPromise(moments['content']['moments_id'])]).then((o:any)=>{
+
+              o[0]?.map((item:any)=>{
+                moments['content']['images'].push(item.image)
+              })
+
+              //根据点赞者id获取点赞者昵称、根据评论者id获取评论者昵称
+              let promiseStatement:any[] = []
+              o[1]?.map((item:any)=>{
+
+                promiseStatement.push(selectUserNamePromise(item['like_user_id']))
+              })
+              o[2]?.map((item:any)=>{
+                promiseStatement.push(selectUserNamePromise(item['comments_user_id']))
+              })
+              Promise.all(promiseStatement).then((usernameArr:any)=>{
+                //将获取到的昵称放入对应位置
+                usernameArr?.map((item:any,index:number)=>{
+                  if(index < o[1].length){
+                    moments['liked'].push(item[0].username)
+                  }
+                  else{
+                    o[2][index - o[1].length]['username'] = item[0].username
+                    moments['comments'].push(o[2][index - o[1].length])
+                  }
+                })
+                //全部数据获取完成，resolve出去
+                resolve(moments)
+              })
+
+            })
+          }
+          //没有朋友圈数据
+          else{
+            resolve(null)
+          }
+        })
+      })
+    }
+
+    userSelectPromise().then((r:any)=>{
+      let promiseStatement:any[] = []
+      r.map((item:any)=>{
+        //根据获取到的用户以及好友id获取朋友圈数据
+        promiseStatement.push(getChatMomentsData(item))
+      })
+      Promise.all(promiseStatement).then((data:any)=>{
+        let allMoments:any[] = []
+        data.map((item:any)=>{
+          //朋友圈数据不为null加入allMoments数组中
+          if(item){
+            allMoments.push(item)
+          }
+        })
+        //所有数据获取完成，反馈给前端
+        res.send({
+          moments:allMoments
+        })
+      })
+    }).catch(()=>{
+      res.send({
+        err:'出现错误了'
+      })
+    })
+  })
   app.use('/',router)
 }
